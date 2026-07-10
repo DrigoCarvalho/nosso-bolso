59f154741c3dd646fadc57afa0b134fcb0f923c1
Rodrigo Carvalho <rodrigocar.lima@gmail.com>
2026-07-09 22:52:15 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nosso-bolso-v1';
+const CACHE = 'nosso-bolso-v2';
 const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
