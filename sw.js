d8fd862d4c5011eee6dc27c7b88f4e08b11d2e6b
Rodrigo Carvalho <rodrigocar.lima@gmail.com>
2026-07-14 09:05:29 -0300

    Update cache version from v5 to v6

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nosso-bolso-v5';
+const CACHE = 'nosso-bolso-v6';
 const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
